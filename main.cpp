476b40ad2cbd726caad0a6570f6a969caf7a9d14
dmitianna <dmitianna@bk.ru>
2026-05-11 23:57:53 +0700

    Размещен исходный предложенный код

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
-#include <QCoreApplication>
+#include "classunit.h"
+#include "methodunit.h"
+#include "printoperatorunit.h"
+std::string generateProgram() {
+    ClassUnit myClass( "MyClass" );
+    myClass.add(
+        std::make_shared< MethodUnit >( "testFunc1", "void", 0 ),
+        ClassUnit::PUBLIC
+        );
+    myClass.add(
+        std::make_shared< MethodUnit >( "testFunc2", "void", MethodUnit::STATIC ),
+        ClassUnit::PRIVATE
+        );
+    myClass.add(
+        std::make_shared< MethodUnit >( "testFunc3", "void", MethodUnit::VIRTUAL |
+                                                              MethodUnit::CONST ),
+        ClassUnit::PUBLIC
+        );
+    auto method = std::make_shared< MethodUnit >( "testFunc4", "void",
+                                               MethodUnit::STATIC );
+    method->add( std::make_shared< PrintOperatorUnit >( R"(Hello, world!\n)" ) );
+    myClass.add( method, ClassUnit::PROTECTED );
+    return myClass.compile();
+}
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
-
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
-
-    return a.exec();
+int main() {
+    std::cout << generateProgram() << std::endl;
+    return 0;
 }
