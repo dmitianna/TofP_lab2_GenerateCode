c851c7ee1761cdd3771a6a6031740543187b3765
dmitianna <dmitianna@bk.ru>
2026-05-14 10:43:01 +0700

    Добавлены modifiers для генерации классов на C#
    Убраны методы, которые будут переопределены с учетом особенностей языка

--- CSclassunit.h
+++ CSclassunit.h
@@ -4,34 +4,26 @@
 #include <vector>
 class CSClassUnit : public ClassUnit
 {
+    enum CSAccesModifier : Flags {
+        PUBLIC = AccessModifier::PUBLIC,
+        PROTECTED = AccessModifier::PROTECTED,
+        PRIVATE = AccessModifier::PRIVATE,
+        PRIVATE_PROTECTED,
+        FILE,
+        INTERNAL,
+        PROTECTED_INTERNAL
+    };
 public:
     explicit CSClassUnit(const std::string &name) : ClassUnit(name)
     { }
 
     void add( const std::shared_ptr< Unit >& unit, Flags flags ) override
     {
-        int accessModifier = PRIVATE;
-        if( flags < ACCESS_MODIFIERS.size() ) {
-            accessModifier = flags;
-        }
-        m_fields[ accessModifier ].push_back( unit );
     }
 
     std::string compile( unsigned int level = 0 ) const override
     {
-        std::string result = generateShift( level ) + "class " + m_name + " {\n";
-        for( size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i ) {
-            if( m_fields[ i ].empty() ) {
-                continue;
-            }
-            result += ACCESS_MODIFIERS[ i ] + ":\n";
-            for( const auto& f : m_fields[ i ] ) {
-                result += f->compile( level + 1 );
-            }
-            result += "\n";
-        }
-        result += generateShift( level ) + "};\n";
-        return result;
+        return {};
     }
 };
 
