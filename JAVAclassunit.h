b83d9048d1397e6fdb476c40e8000f767ddce3ab
dmitianna <dmitianna@bk.ru>
2026-05-14 17:18:45 +0700

    Созданы заготовки для Java
    Пока ничего нового, код сдублирован с cpp

---
+++ JAVAclassunit.h
@@ -0,0 +1,39 @@
+#ifndef JAVACLASSUNIT_H
+#define JAVACLASSUNIT_H
+#include "classunit.h"
+#include "JAVAmethodunit.h"
+#include <vector>
+class JAVAClassUnit : public ClassUnit
+{
+public:
+    explicit JAVAClassUnit(const std::string &name) : ClassUnit(name)
+    { }
+
+    void add( const std::shared_ptr< Unit >& unit, Flags flags ) override
+    {
+        int accessModifier = PRIVATE;
+        if( flags < ACCESS_MODIFIERS.size() ) {
+            accessModifier = flags;
+        }
+        m_fields[ accessModifier ].push_back( unit );
+    }
+
+    std::string compile( unsigned int level = 0 ) const override
+    {
+        std::string result = generateShift( level ) + "class " + m_name + " {\n";
+        for( size_t i = 0; i < ACCESS_MODIFIERS.size(); ++i ) {
+            if( m_fields[ i ].empty() ) {
+                continue;
+            }
+            result += ACCESS_MODIFIERS[ i ] + ":\n";
+            for( const auto& f : m_fields[ i ] ) {
+                result += f->compile( level + 1 );
+            }
+            result += "\n";
+        }
+        result += generateShift( level ) + "};\n";
+        return result;
+    }
+};
+
+#endif // JAVACLASSUNIT_H
